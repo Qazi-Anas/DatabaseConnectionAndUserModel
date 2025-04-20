68f7b2ee311ea88af31db51da16308ce47710fe2
unknown <qazianas1995@uop.edu.pk>
2025-04-20 09:26:54 -0700

    successfully connect to mongoDb server

--- index.js
+++ index.js
@@ -8,12 +8,21 @@ dotenv.config({
 
 const app = express()
 
+//some important middleware use to parse json object, data comes from url and static pages
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(express.static(`${__dirname}/${'public'}`))
 
 
+//mongoose database connection
 
-app.listen(process.env.PORT, () => {
-    console.log(`App is listen to port ${process.env.PORT}`)
+const connectToDatabase = mongoose.connect(`${process.env.DATABASE_CONNECTION_STRING}/${process.env.DATABASE_NAME}`).then( () => {
+    console.log("Connect to database successfully!")
+    app.listen(process.env.PORT, () => {
+        console.log(`App is listen to port ${process.env.PORT}`)
+    })
+}).catch( (error) => {
+    console.log("Failed to connect to database !! Shutting Down")
+    process.exit(1)
 })
+
