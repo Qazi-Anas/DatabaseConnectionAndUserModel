5916921ce6aadd80b04cbcf7bc5d914d26e9cef9
unknown <qazianas1995@uop.edu.pk>
2025-04-20 11:10:45 -0700

    create user model and store some documents in database

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const dotenv = require("dotenv")
 const mongoose = require("mongoose")
+const User = require("./models/userModel")
 
 dotenv.config({
     path: "./.env"
@@ -18,11 +19,30 @@ app.use(express.static(`${__dirname}/${'public'}`))
 
 const connectToDatabase = mongoose.connect(`${process.env.DATABASE_CONNECTION_STRING}/${process.env.DATABASE_NAME}`).then( () => {
     console.log("Connect to database successfully!")
-    app.listen(process.env.PORT, () => {
-        console.log(`App is listen to port ${process.env.PORT}`)
-    })
 }).catch( (error) => {
     console.log("Failed to connect to database !! Shutting Down")
     process.exit(1)
 })
 
+app.post("/api/v1/users/signup", async (req, res, next) => {
+    const newUser = await User.create({
+        name: req.body.name,
+        email: req.body.email,
+        role: req.body.role,
+        password: req.body.password,
+        passwordConfirm: req.body.passwordConfirm 
+    })
+
+    res
+    .status(201)
+    .json({
+        status: 'success',
+        data: {
+            user: newUser
+        }
+    })
+})
+
+app.listen(process.env.PORT, () => {
+    console.log(`App is listen to port ${process.env.PORT}`)
+})
---
+++ models/userModel.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose")
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Name is required']
+    },
+    email: {
+        type: String,
+        required: [true, 'Email is required'],
+        unique: true
+    },
+    role: {
+        type: String,
+        enum: ['user', 'admin'],
+        default: 'user'
+    },
+    password: {
+        type: String,
+        required: [true, 'Password is required']
+    },
+    passwordConfirm: {
+        type: String,
+        required: [true, 'Confirm password is required']
+    }
+}, {timestamps: true})
+
+const User = mongoose.model("User", userSchema)
+
+module.exports = User
+
+
